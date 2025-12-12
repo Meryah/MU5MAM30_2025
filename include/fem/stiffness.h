b9dda71f7e76d8015412bb594df99a2bc9d9b9b2
Alberto Taddei <albertotaddei1@gmail.com>
2025-12-12 17:28:10 +0100

    filled stiffness header file

--- include/fem/stiffness.h
+++ include/fem/stiffness.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "vec3.h"
+#include "sys_utils.h"
 
 /* Given a triangle ABC, computes the (symmetric) 3x3 stiffness matrix S s.t.
  *
@@ -23,5 +24,22 @@
  */
 void inline stiffness(const Vec3d &AB, const Vec3d &AC, double *__restrict S)
 {
-	/* Your implementation goes here */
+    const Vec3d n   = cross(AB, AC);
+    const double nn = dot(n, n);
+    ASSERT_ALWAYS(nn > 0.0);
+
+    const double area = 0.5 * std::sqrt(nn);  // |K| = 1/2 ||AB x AC||
+    const double inv_nn = 1.0 / nn;
+
+    // gA = grad(phi_A), gB = grad(phi_B), gC = grad(phi_C)
+    const Vec3d gA = cross(n, (AB - AC)) * inv_nn; // n x (B-C)
+    const Vec3d gB = cross(n, AC) * inv_nn;        // n x (C-A)
+    const Vec3d gC = cross(n, (-AB)) * inv_nn;     // n x (A-B)
+
+    S[0] = area * dot(gA, gA); // 00
+    S[1] = area * dot(gB, gB); // 11
+    S[2] = area * dot(gC, gC); // 22
+    S[3] = area * dot(gA, gB); // 01
+    S[4] = area * dot(gB, gC); // 12
+    S[5] = area * dot(gC, gA); // 20 (=02)
 }
