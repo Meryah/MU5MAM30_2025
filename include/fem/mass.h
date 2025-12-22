472028f15b187889c1ecb218737167583d843c78
Lyna <Lynouch@MacBook-Pro-de-Lyna.local>
2025-12-22 17:37:20 +0100

    Fonction mass MAJ

--- include/fem/mass.h
+++ include/fem/mass.h
@@ -20,24 +20,15 @@
 
 void inline mass(const Vec3d &AB, const Vec3d &AC, double *__restrict M)
 {
-<<<<<<< HEAD
 	/*On calcule le déterminant de la Jacobienne*/
 	/*Même si cross est le pdt vectoriel en 3D on peut écrire (x,y,0) et comme on prend la norme on a exactement ce qu'on veut*/
-=======
-	/*On calcule le determinant de la Jacobienne*/
-	/*Meme si cross est le pdt vectoriel en 3D on peut ecrire (x,y,0) et comme on prend la norme on a exactement ce qu'on veut*/
->>>>>>> eb4e3af (MAJ mass.h)
     
 	double detJacobienne = norm(cross(AB,AC));
 	
 
 	if (detJacobienne == 0.0)
 	{
-<<<<<<< HEAD
 		 std::cout << "Les points sont alignés : on n'a pas un triangle !" << std::endl;
-=======
-		 std::cout << "Les points sont alignes : on n'a pas un triangle !" << std::endl;
->>>>>>> eb4e3af (MAJ mass.h)
 	}
 
 	else {
